b269aa1c771063e007a03cd9cda9344d58980255
Acelyn <Acelyn@users.noreply.github.com>
2020-04-10 20:50:19 -0400

    Done

--- Lexer.py
+++ Lexer.py
@@ -2,221 +2,145 @@ import sys
 from typing import Generator, Tuple
 import re
 
-"""
-. - the dot character matches any character
-#.* - searched for a comment
-\s*#.* - comments with whitespace in front including newlines
-^ - matches the start of a line
-^[ \t]*#.* - look for comments that start a line
-^[ \t]*#.*$ - match single line comment
-cl.[ds] - match clud and clas
-Replace single line comments with a triple quoted string version.
-^[ \t]*#(.*)  - single line comment but capture the comment content
-                parentheses in a regular expression represent a "capture group"
-
-[_a-zA-Z][_a-zA-Z0-9]*   - match an identifier
-\w is shorthand for [_a-zA-Z0-9]
-[_a-zA-Z]\w*
-What are the rules for an integer constant?
-[0-9]+ - integer constant
-0[xX][0-9a-fA-F]+   - a hexadecimal constant
-"""
-#  """\n$1\n""" - replacement text bos where $1 represents the captured value
-
-# sample comment
-
-"""
- sample comment
-"""
-
 
 class Lexer:
     # class variables
-
+    # create a dictionary to present tokens
     d = {} # empty dictionary
 
+    # literal string
+    d["Literal String"] =r"(\"(?:[^\\\"]|\\.)*\")"
+
     # Integer:
-    d["\d+|\d+[_\d]*\d+"] = "Integer"
+    d["Integer"] = "\d+|\d+[_\d]*\d+"
 
     # real number
-    d["\d+([_\d]*\d+)?\.?(\d+([_\d]*\d+)?)?e?[\+-]?\d+([_\d]*\d+)?"] = "real number"
+    d["real number"] = "\d+([_\d]*\d+)?\.?(\d+([_\d]*\d+)?)?e?[\+-]?\d+([_\d]*\d+)?"
 
-    # True/False
-    d["True|true|False|false"] = "Keyword"
-
-    # other keyword
-    d["print|bool|else|false|if|true|float|int|while|main|char"] = "Keyword"
-
-    # String Literal
-    d["^\"\w*\"$"] = "String Literal"
+    # Keyword
+    d["Keyword"] = "True|true|False|false|print|bool|else|false|if|true|float|int|while|main|char"
 
     # operators
-    d["\|\|"] = "or"
-    d["&&"] = "and"
-    d["=="] = "euqal-equal"
-    d["!="] = "not-equal"
-    d["<"] = "smaller"
-    d["<="] = "smaller-equal"
-    d[">"] = "greater"
-    d[">="] = "greater-equal"
-    d['(\+)'] = "plus"
-    d['(-)'] = "minus"
-    d["\*"] = "multiply"
-    d["/"] = "devide"
-    d["="] = "assignment"
-    d["%"] = "mod"
-    d["!"] = "exclamation"
-    d[";"] = "semicolon"
-    d[","] = "comma"
-    d["{"] = "L{"
-    d["}"] = "R}"
-    d["(\()"] = "Lparen"
-    d["(\))"] = "Rparen"
-    d["\["] = "L["
-    d["\]"] = "R["
-    d[">>"] = "shift right"
-    d["<<"] = "shift left"
+    d["or"] = "\|\|"
+    d["and"] = "&&"
+    d["euqal-equal"] = "=="
+    d["not-equal"] = "!="
+    d["smaller"] = "<"
+    d["smaller-equal"] = "<="
+    d["greater"] = ">"
+    d["greater-equal"] = ">="
+    d["plus"] = "(\+)"
+    d["minus"] = '(-)'
+    d["multiply"] = "\*"
+    d["devide"] = "/"
+    d["assignment"] = "="
+    d["mod"] = "%"
+    d["exclamation"] = "!"
+
+    #punctuation
+    d["semicolon"] = ";"
+    d["comma"] = ","
+    d["LCbracket"] = "{"
+    d["RCbracket}"] = "}"
+    d["Lparen"] = "(\()"
+    d["Rparen"] = "(\))"
+    d["LSbracket"] = "\["
+    d["RSbracket["] = "\]"
+    d["shift right"] = ">>"
+    d["shift left"] = "<<"
 
     # Identifier
-    d["^[a-zA-Z_]\w*"] = "Identifier"
-
-    """
-    w = {}
-    w["\d+_"] = "Error : underscore at the end of integer"
-    w["(\d+([_\d]*\d+)?\.)*(\d+([_\d]*\d+)?)?e?[\+-]?\d+([_\d]*\d+)?"] = "Error: multiple dots"
-    """
+    d["Identifier"] = "^[a-zA-Z_]\w*"
 
 
 
-
-
-
-
-    '''
-    INTLIT = 0  # codes for the "kind" of value
-    PLUS = 1
-    ID = 2
-    LPAREN = 3
-    RPAREN = 4
-    INTEGER = 5
-'''
-    # fn - file name we are lexing
     def __init__(self, arg: list):
-        if len(arg)>1:
+        # if there are 2 system argument(one of them is Lexer.py), run the program
+        if len(arg) == 2:
             try:
                 self.f = open(arg[1])
+            # throw IOError if file name is wrong/ file does not exist
             except IOError:
                 print("File {} not found".format(arg[1]))
                 print("Exiting")
                 sys.exit(1)  # can't go on
+        # exit with error message if there are more than one input files
+        elif len(arg) > 2:
+            print("Expect 1 argument, given ", len(arg)-1)
+            print("Exiting")
+            sys.exit(1)
+        # exit with error message if there is not input file
         else:
             print("there is not input file")
             print("Exiting")
             sys.exit(1)
-		
 
     def token_generator(self) -> Generator[Tuple[int, str, int], None, None]:
         """
         Returns the tokens of the language
         """
 
-        # backslash plague - eliminated because of python raw strings
-        split_patt = re.compile(r"(\+)|\s|(\()|(\))")  # parentheses around a pattern
-        # captures the value
-
         # a more readable way to write the split
         # pattern above using the VERBOSE option
         split_patt = re.compile(
             r"""             # Split on 
-               ((?<!e)\+) |        #  plus and capture
-               ((?<!e)-) |         #  minus and capture, minus not special unless in []
+               (\"(?:[^\\\"]|\\.)*\")|   # Literal String   
+               [ \t]*//.*|               # comment
+               ((?<!e)\+) |              #  plus and capture
+               ((?<!e)-) |               #  minus and capture, minus not special unless in []
                ([=><!]=|=)  |   # ==, >=, <=, != and capture
-              #(".+~\"$) |    # Literal String
                \s   |        #  whitespace
                (\() |        #  left paren and capture
                (\))  |       #  right paren and capture
-               ({)   |      # right { and capture
-               (})   |      # left } and capture
-               (\[)   |      # right [ and capture
-               (\])   |     # left ] and capture
+               ({)   |      # right curly brackets and capture
+               (})   |      # left curly brackets and capture
+               (\[)   |      # right square brackets and capture
+               (\])   |     # left square brackets and capture
                (,)    |     # comma and capture
-               (;)    |     #semicolon and capture
-               (!)    |     #exclamation and capture
-               (%)  |
-               (\*) |
-               (/)  |
-               (>>)  |
-               (<<) 
-              
+               (;)    |     # semicolon and capture
+               (!)    |     # exclamation and capture
+               (%)  |       # mod and capture
+               (\*) |       # star and capture
+               (/)  |       # slash and capture
+               (>>)  |      # shift right and capture
+               (<<)         #shift left and capture
             """,
             re.VERBOSE
         )
-
-
+        # go through every line in the input file
         for i, line in enumerate(self.f, start=1):
-            # throw comment
-            # if there is "...//..."
-            if re.search("\".*?//.*?\"",line)!=None:
-                line = re.sub("((.*?\".*?\".*?)+)//.*", "\g<1>", line)
-            # if there is not "...//..."
-            else:
-                line = re.sub("(.*?)//.*", "\g<1>", line)
-
-
-            String_pattern = re.compile("(?<!\\\)\".*?(?<!\\\)\"")
-
-            literal_string = String_pattern.finditer(line)
-            if (literal_string != None):
-                for c in literal_string:
-                      yield  "Literal String" , line[c.start():c.end()], i
-            else:
-                print("None")
-
-
-          #  yield
-            line = re.sub("(?<!\\\)\".*?(?<!\\\)\"", " ", line)
-           # print(line," on line ",i)
+            #  go through every token in line
             tokens = (t for t in split_patt.split(line) if t)
             for t in tokens:
-            #try:
-                for k,v in Lexer.d.items():
-                    if re.fullmatch(k,t)!= None:
-                        yield v, t, i
+                # search for match-pattern in the dictionary
+                for k, v in Lexer.d.items():
+                    # if it match, yield token,name and line #
+                    if re.fullmatch(v, t) != None:
+                        yield k, t, i
                         break
+                # if it can not match any pattern in the dictionary, yield error message and line #
                 else:
-                    yield "None", t , i
-
-
-
-                #except:
-                   # print("I can't find this")
-
-
+                    yield "Error: illegal token", t, i
 
 
 if __name__ == "__main__":
-    b = "\"a string // literal\" \"// and another\"   // two"
-    print(re.fullmatch("((.*?\".*?\".*?)+)//.*", b))
-    print(re.fullmatch("(.*(.* \".*\".)*)//.*","print(\" // the tokens ; ) 12345\")"))
-
-    s = "He said \"go\""
 
     lex = Lexer(sys.argv)
-   # lex = Lexer("test2.sluc")
 
     g = lex.token_generator()
 
-    print("%-35s %-30s %s" % ("Token", "Name", "Line Number"))
-    print("--------------------------------------------------------------------------------------")
-
+    # print the header for the output table
+    print("%-35s %-85s %s" % ("Token", "Name", "Line Number"))
+    print(
+        "--------------------------------------------------------------------------------------------------------------------------------------")
+    # setup a list for the interval between column
+    format_lst = ["%-35s", "%-80s", "%s"]
 
     while True:
-
-
-
         try:
-            for i in next(g):
-               print("%-25s"%(i), "\t\t\t",end="")
+            # print output table
+            for j, i in enumerate(next(g), start=0):
+                print(format_lst[j] % (i), "\t", end="")
             print("")
         except StopIteration:
             print("Done")
